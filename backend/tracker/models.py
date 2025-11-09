ecc3b94cfebd436266131581adb8a57e403b819a
KingARTHUR3682 <arthurkbx@gmail.com>
2025-11-09 13:49:23 +0800

    Add tracker app with models, migrations, and serializers

---
+++ backend/tracker/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+# Expense model
+class Expense(models.Model):
+    name = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ backend/tracker/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Category, Expense
+from django.contrib.auth.models import User
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+        def create(self, validated_date):
+            user = User.objects.create_user(
+                username = validated_date['username'],
+                email = validated_date['email'],
+                password = validated_date['password']
+            )
+            return user
