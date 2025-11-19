89ca75d39836b13419f6f04b38981d85d0ba122a
KingARTHUR3682 <arthurkbx@gmail.com>
2025-11-19 18:53:59 +0800

    chore: Changed the access token to store on local storage

--- frontend/app/src/stores/auth.js
+++ frontend/app/src/stores/auth.js
@@ -3,7 +3,8 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', () => {
 // -- State --
-    const token = ref(null)
+    // Initialize state from local storage if it exists
+    const token = ref(localStorage.getItem('access_token') || null)
 
 // -- Getter --
     const isAuthenticated = ref(!!token.value) // !! converts the value to boolean
@@ -13,12 +14,14 @@ export const useAuthStore = defineStore('auth', () => {
     function setToken(newToken) {
         token.value = newToken
         isAuthenticated.value = true
+        localStorage.setItem('access_token', newToken)
     }
 
     // Function to clear the token when a user log out
     function logout() {
         token.value = null
         isAuthenticated.value = false
+        localStorage.removeItem('access_token')
     }
 
     return { token, isAuthenticated, setToken, logout }
