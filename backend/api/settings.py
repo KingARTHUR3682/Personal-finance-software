e19a775f8add974de9d5ae823b8d64a3d3b30674
IDKAT7USAY <liuyongle423@gmail.com>
2025-11-23 18:46:07 +0800

    fix: reset password function error

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -179,7 +179,16 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'haakpfs.official@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'bmpertoswkgxcudp'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 FRONTEND_URL = 'http://192.168.100.40:5173'
 
